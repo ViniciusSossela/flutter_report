a7ff8913d27b072ef75ca31ef7eb4ad817057d3e
Vinicius Sossella <viniciussossella@gmail.com>
2021-06-21 14:01:38 -0300

    improved files share

--- lib/src/share/native_share.dart
+++ lib/src/share/native_share.dart
@@ -1,11 +1,13 @@
-import 'package:esys_flutter_share/esys_flutter_share.dart';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 Future<void> share(List<int> encodedData, String fileName, String fileExtension,
-        {String desc = ''}) async =>
-    await Share.file(
-      fileName,
-      '$fileName.$fileExtension',
-      encodedData,
-      '*/*',
-      text: desc,
-    );
+    {String desc = ''}) async {
+  final tempDir = await getTemporaryDirectory();
+  final file = await File('${tempDir.path}/$fileName.$fileExtension').create();
+  await file.writeAsBytes(encodedData);
+
+  await Share.shareFiles([file.path], mimeTypes: ['*/*'], subject: desc);
+}
