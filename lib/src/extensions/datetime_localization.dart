1f9f32960226914de82549e0d4492db8ebf4d26c
vinicius sossella <viniciussossella@gmail.com>
2023-05-19 10:51:02 -0300

    code upgrades and fix

--- lib/src/extensions/datetime_localization.dart
+++ lib/src/extensions/datetime_localization.dart
@@ -9,30 +9,22 @@ const ptDateFormat = 'dd/MM/yyyy';
 
 extension DateTimeFormatExtension on DateTime {
   String parseToLocalizedDateTime({String locale = enUS}) {
-    assert(locale != null && this != null);
-
     switch (locale) {
       case enUS:
         return DateFormat(enUSDatetimeFormat).format(this);
-        break;
       case pt:
         return DateFormat(ptDatetimeFormat).format(this);
-        break;
       default:
         return DateFormat(enUSDatetimeFormat).format(this);
     }
   }
 
   String parseToLocalizedDate({String locale = enUS}) {
-    assert(locale != null && this != null);
-
     switch (locale) {
       case enUS:
         return DateFormat(enUSDateFormat).format(this);
-        break;
       case pt:
         return DateFormat(ptDateFormat).format(this);
-        break;
       default:
         return DateFormat(enUSDateFormat).format(this);
     }
